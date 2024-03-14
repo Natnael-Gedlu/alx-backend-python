1a7595d9fd11ad668fc846117003872f8e9828e3
Natnael-Gedlu <nategd800@gmail.com>
2024-03-14 17:32:52 +0300

    fixed pycodestyle error

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,6 +4,7 @@ Type-annotated function that takes
 two arguments and returns their sum.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Adds two floating-point numbers and returns their sum.
