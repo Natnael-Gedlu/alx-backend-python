d01ee258f7cacc8669621928238fff4f20bcee08
Natnael-Gedlu <nategd800@gmail.com>
2024-03-14 17:31:36 +0300

    Implemented Type-annotated function that takes two arguments and returns their sum

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function that takes
+two arguments and returns their sum.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns their sum.
+
+    Args:
+        a (float): The first floating-point number.
+        b (float): The second floating-point number.
+
+    Returns:
+        float: The sum of `a` and `b`.
+    """
+    return a + b
