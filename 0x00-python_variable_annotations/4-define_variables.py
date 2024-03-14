47786fd885c1200998641a748f765c0b683d4842
Natnael-Gedlu <nategd800@gmail.com>
2024-03-14 18:40:45 +0300

    Initialization of variables with type annotations and initial values

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Initialization of variables with type annotations and initial values.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
