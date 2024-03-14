bf0b2ebbc3903e7907098569520a78ae00829b2e
Natnael-Gedlu <nategd800@gmail.com>
2024-03-14 18:00:29 +0300

    Implemented a function for converting a float to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a function for converting a float to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to its string representation.
+
+    Args:
+        n (float): The floating-point number.
+
+    Returns:
+        str: The string representation of `n`.
+    """
+    return str(n)
