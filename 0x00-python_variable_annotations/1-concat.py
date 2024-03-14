5020f5e48b87f3b1c923b67b418c9441ad0ca505
Natnael-Gedlu <nategd800@gmail.com>
2024-03-14 17:45:50 +0300

    Implemented a function for concatenating two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a function for concatenating two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
